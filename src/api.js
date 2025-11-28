3d4528cfa72b47f57de814c0d343e3682d95cbd0
BaconKage <152167782+BaconKage@users.noreply.github.com>
2025-11-29 02:01:53 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,16 +1,10 @@
 import axios from "axios";
 
-// IMPORTANT: your backend's base URL
 const API = axios.create({
   baseURL: "https://my-gym-admin-backend.onrender.com/api",
 });
 
 export async function fetchDashboardStats() {
   const res = await API.get("/dashboard");
-  return res.data;
-}
-
-export async function fetchCollectionsMeta() {
-  const res = await API.get("/meta");
-  return res.data;
+  return res.data; // { totalActivities, totalDailyStepsRecords, totalExercises, activeChallenges, openCarts, totalConversations }
 }
